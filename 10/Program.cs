3088deafcff3d6c45574f2790aeeeb462895c509
AnastasiyaM <anastasiya.smirnova96@yandex.ru>
2022-07-15 11:32:04 +0300

    Задача 10

---
+++ 10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = Convert.ToInt32(Console.ReadLine());
+int second = (number / 10) % 10;
+
+if ((number > 99) && (number < 1000)) {
+   Console.Write(second); 
+}
+else {
+   Console.Write("Ошибка");  
+}
